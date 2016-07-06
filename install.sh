8a1948e4e9d97a3923589c067466f844e6d8b312
alexey-svistunov <alexeysvistunov@gmail.com>
2016-07-06 12:44:10 +0300

    Add magento2 composer file, Update install.sh script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 echo "Enter demo name:"
-demoName=FilmSlider
-# read demoName
+#demoName=magento21full
+read demoName
+
+echo "Enter host name:"
+#hostName=chris.magento2.dev
+read hostName
+
 dirName=demo_$demoName
 
 mkdir -p $dirName
@@ -10,12 +15,12 @@ cp composer.json $dirName/
 chown -R $USER:www-data $dirName/
 
 cd $dirName/
-# composer update
+composer update
 
 databaseName=filmslider
 
-mysql -u root -e "DROP DATABASE IF EXISTS demo_$databaseName"
-mysql -u root -e "CREATE DATABASE demo_$databaseName"
+mysql -u root -proot -e "DROP DATABASE IF EXISTS demo_$databaseName"
+mysql -u root -proot -e "CREATE DATABASE demo_$databaseName"
 
 chown -R $USER:www-data .
 
@@ -24,7 +29,7 @@ chmod -R 777 pub/static
 chmod -R 777 pub/media
 chmod -R 777 var/
 
-# bin/magento setup:install --base-url="http://192.168.9.18/$dirName" --db-host=localhost --db-name="demo_$databaseName" --db-user=root --admin-firstname=admin --admin-lastname=User --admin-email=admin@admin.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1 --backend-frontname=admin
+php bin/magento setup:install --base-url="http://$hostName/$dirName" --db-host=localhost --db-name="demo_$databaseName" --db-user=root --db-password=root --admin-firstname=admin --admin-lastname=User --admin-email=admin@admin.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1 --backend-frontname=admin
 
 chown -R $USER:www-data .
 chmod -R 777 var/
